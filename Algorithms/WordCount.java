c6888d473000c9a14a955c662f12c7709db549b8
Sumit Sourav Choudhury <95333079+SumitSouravChoudhury@users.noreply.github.com>
2025-03-04 12:47:00 +0530

    Update WordCount.java

--- Algorithms/WordCount.java
+++ Algorithms/WordCount.java
@@ -1,1 +1,42 @@
+import java.util.*;
 
+public class WordCount {
+
+  public static int wordCount(String str) {
+    if(str.isEmpty() || str==null) {
+            return 0;
+        }
+        
+        int count = 0;
+        boolean isWord = false;
+        
+        for(int i=0; i<str.length(); i++) {
+            
+            char ch = str.charAt(i);
+            
+            if(ch != ' ') {
+                if(!isWord) {
+                    count++;
+                    isWord = true;
+                }
+            }
+            else {
+                isWord = false;
+            }
+        }
+        
+        return count;
+  }
+  
+  public static void main(String[] args) {
+
+    Scanner s = new Scanner(System.in);
+
+    String str = s.nextLine();
+    
+    int count = wordCount(str);
+    System.out.println(count);
+    
+    s.close();
+  }
+}
